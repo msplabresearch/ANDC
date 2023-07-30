9f7227170dab35b43cc35022a96a234398765dd5
3loi <43795050+3loi@users.noreply.github.com>
2023-07-30 01:21:43 -0500

    Delete env_setup directory

--- env_setup/env_init.sh
+++
@@ -1,5 +0,0 @@
-conda env create python=3.7.3 -f winston_base_cpu.yml  
-conda env create python=3.7.13 -f HF.yml  
-conda env create python=3.10.4 -f tf2_cpu.yml  
-conda env create python=3.7.0 -f ssl.yml
-conda env create python=3.7.0 -f wav2vec.yml
