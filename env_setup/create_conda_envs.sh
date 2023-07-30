8cd463f69d74244058eab32679132baa0702d88d
3loi <43795050+3loi@users.noreply.github.com>
2023-07-30 01:22:39 -0500

    Add files via upload

--- env_setup/create_conda_envs.sh
+++ env_setup/create_conda_envs.sh
@@ -1,1 +1,5 @@
-test
+conda env create python=3.7.3 -f ANDC_base_cpu.yml  
+conda env create python=3.7.13 -f ANDC_HF.yml  
+conda env create python=3.10.4 -f ANDC_tf2_cpu.yml  
+conda env create python=3.7.0 -f ANDC_ssl.yml
+conda env create python=3.7.0 -f ANDC_wav2vec.yml
