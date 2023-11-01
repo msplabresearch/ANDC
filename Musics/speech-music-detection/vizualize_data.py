392f6236183e5ce728158f66bf1417d12a7b5453
3loi <43795050+3loi@users.noreply.github.com>
2023-11-01 13:04:51 -0400

    Delete Musics/speech-music-detection directory

--- Musics/speech-music-detection/vizualize_data.py
+++
@@ -1,9 +0,0 @@
-import matplotlib
-matplotlib.use('TkAgg')
-
-import smd.data.display.events as display
-
-AUDIO_PATH = "./total4_m.wav"
-ANNOTATION_PATH = "./total4.txt"
-
-display.audio_with_events(AUDIO_PATH, ANNOTATION_PATH)
